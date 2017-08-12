c0d1d9a6083baa6de6380bd6fbb9f7b53d4a6dde
Giorgio Aquino <gigetto@gmail.com>
2017-08-12 13:54:40 +0200

    Add sound, button socket and random numbers

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React, { Component } from 'react';
-import TWEEN from 'tween.js';
+import React, { Component } from "react";
 
-import Number from './Number';
+import io from 'socket.io-client';
 
-import './App.css';
+import TWEEN from "tween.js";
+import beepAudio from '../assets/beep.mp4';
+
+
+
+import Number from "./Number";
+
+import "./App.css";
 
 function shuffle(originalArray) {
-    let array = originalArray.slice();
-    let counter = array.length;
+  let array = originalArray.slice();
+  let counter = array.length;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+    // Decrease counter by 1
+    counter--;
 
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
 
-    return array;
+  return array;
 }
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
-    const numbers = Array.apply(null, Array(100)).map( (e, i) => i+1);
-    const turnNumbers = numbers.concat(numbers, numbers, numbers, numbers, numbers);
+    const numbers = shuffle(Array.apply(null, Array(100)).map((e, i) => i + 1));
+    const turnNumbers = numbers.concat(
+      numbers,
+      numbers,
+      numbers,
+      numbers,
+      numbers
+    );
 
     this.state = {
       numbers,
@@ -46,23 +57,38 @@ class App extends Component {
 
     this.animate = this.animate.bind(this);
 
+    this.audio = new Audio(beepAudio);
+
+    this.io = io('http://raspberrypi.local:8080');
+    this.io.on('button', (buttons) => {
+      console.log(buttons)
+
+      if (buttons.button1 && buttons.button1 === 1) {
+        // this.start();
+        this[!this.state.animating ? "start" : "stop"]();
+        
+      }
+    });
+
+
   }
 
   componentDidMount() {
-    window.addEventListener('keydown', this.onKeyDown.bind(this));
-    window.addEventListener('dblclick', this.onDblClick.bind(this));
+    window.addEventListener("keydown", this.onKeyDown.bind(this));
+    window.addEventListener("dblclick", this.onDblClick.bind(this));
     this.animate();
   }
 
-  onKeyDown(e){
+  onKeyDown(e) {
     // e.preventDefault();
     // start|stop on [space]
 
-    switch(e.keyCode) {
+    switch (e.keyCode) {
       case 32:
         e.preventDefault();
-        this[!this.state.animating ? 'start' : 'stop']();
-      break;
+        this[!this.state.animating ? "start" : "stop"]();
+        break;
+        default:
     }
 
     // reset on [escape]
@@ -70,17 +96,17 @@ class App extends Component {
   }
 
   onDblClick() {
-    this[!this.state.animating ? 'start' : 'stop']();
+    this[!this.state.animating ? "start" : "stop"]();
   }
 
   start() {
-
     const extractionDuration = 18100;
 
     if (this.state.animating) return;
 
     const randomNumber = this.state.issuedNumber.pop() - 1;
-    const totalnumberLenght = this.state.turnNumbers.length - this.state.numbers.length; 
+    const totalnumberLenght =
+      this.state.turnNumbers.length - this.state.numbers.length;
 
     const shuoldIssue = this.state.numbers[randomNumber];
 
@@ -90,127 +116,180 @@ class App extends Component {
       this.t.stop();
     }
 
-    this.state.lastIssuedNumber
+    this.t = new TWEEN.Tween({ pos: this.state.lastIssuedNumber }).to(
+      { pos: totalnumberLenght + randomNumber },
+      extractionDuration
+    );
 
-    this.t = new TWEEN.Tween({ pos: this.state.lastIssuedNumber })
-                  .to({ pos: totalnumberLenght + randomNumber }, extractionDuration)
-                  
     // // this.t.interpolation( TWEEN.Interpolation.Linear );
     // this.t.easing(TWEEN.Easing.Quadratic.InOut);
     this.t.easing(TWEEN.Easing.Exponential.InOut);
     // this.t.easing(TWEEN.Easing.Cubic. Out);
 
     let _self = this;
+    let lastPos = null;
+    let minPlayDistance = 0.001;
+    let lastDelta = 0;
 
-    this.t.onUpdate(function (delta) {
-      
+    this.t.onUpdate(function(delta) {
       let currentPos = Math.floor(this.pos);
+
+      if ((lastPos === null || lastPos !== currentPos) && delta - lastDelta >= minPlayDistance) {
+        lastPos = currentPos
+        _self.audio.play();
+        // console.log(delta - lastDelta, minPlayDistance, (delta - lastDelta >= minPlayDistance))
+        
+        lastDelta = delta;
+
+      }
+
+      
 
       _self.setState({
         currentPos,
         animationPos: delta
       });
+
     });
 
     this.t.onComplete(() => {
-      this.stop()
-        .then(() => {
-          console.log( shuoldIssue , this.state.currentPos )
-          if( (shuoldIssue !== this.state.turnNumbers[this.state.currentPos])) {
-            console.error(shuoldIssue , this.state.turnNumbers[this.state.currentPos]);
-          }
-          this.exctractNumber();
-        })
-        // .then(() => {
-        //   if (this.state.issuedNumber.length < 100) {
-        //     this.start();
-        //   } else {
-        //     console.log(this.state.issuedNumber.sort( (a, b) => a - b));
-        //   }
-        // })
+      this.stop().then(() => {
+        // console.log(shuoldIssue, this.state.currentPos);
+        if (shuoldIssue !== this.state.turnNumbers[this.state.currentPos]) {
+          console.error(
+            shuoldIssue,
+            this.state.turnNumbers[this.state.currentPos]
+          );
+        }
+        // this.exctractNumber();
+      });
+      // .then(() => {
+      //   if (this.state.issuedNumber.length < 100) {
+      //     this.start();
+      //   } else {
+      //     console.log(this.state.issuedNumber.sort( (a, b) => a - b));
+      //   }
+      // })
     });
 
-    this.setState({
-      animating: true,
-      lastIssuedNumber: randomNumber
-    }, () => {
-      this.t.start();
-    });
-
+    this.setState(
+      {
+        animating: true,
+        lastIssuedNumber: randomNumber
+      },
+      () => {
+        this.t.start();
+      }
+    );
   }
 
   stop() {
-
-    console.log("stop")
+    console.log("stop");
     return new Promise(resolve => {
-      this.setState({
-        animating: false
-      },resolve);
+      this.setState(
+        {
+          animating: false
+        },
+        resolve
+      );
     });
-
   }
 
-  exctractNumber() {
+  // exctractNumber() {
+  //   // turnNumbers: shuffle(numbers),
 
-   // turnNumbers: shuffle(numbers),
+  //   // const number = this.state.turnNumbers.pop();
+  //   // const number = this.state.turnNumbers[this.state.turnNumbers.currentPos];
+  //   // this.state.issuedNumber.push(number);
 
-    // const number = this.state.turnNumbers.pop();
-    const number = this.state.turnNumbers[this.state.turnNumbers.currentPos];
-    // this.state.issuedNumber.push(number);
-
-    return new Promise(resolve => {
-      this.setState({
-        // turnNumbers: shuffle(this.state.turnNumbers),
-        // issuedNumber: this.state.issuedNumber
-      }, resolve);
-    })
-  }
+  //   return new Promise(resolve => {
+  //     this.setState(
+  //       {
+  //         // turnNumbers: shuffle(this.state.turnNumbers),
+  //         // issuedNumber: this.state.issuedNumber
+  //       },
+  //       resolve
+  //     );
+  //   });
+  // }
 
   animate() {
-    requestAnimationFrame(this.animate)
-    
-    if (this.state.animating) {
-      TWEEN.update();      
-    } 
+    requestAnimationFrame(this.animate);
 
+    if (this.state.animating) {
+      TWEEN.update();
+    }
   }
 
   render() {
-
     const timerAnimation = {
       // width: (this.state.animationPos * 100) + "%"
       transform: `scaleX(${this.state.animationPos})`
     };
 
-    const wheelFigureClass = this.state.animating ? 'wheel__figure wheel__figure--start-rotation' : '';
+    const wheelFigureClass = this.state.animating
+      ? "wheel__figure wheel__figure--start-rotation"
+      : "";
 
     return (
       <div className="App">
-        
         <div className="numbers">
-
           {/*{ this.state.currentPos }*/}
 
-            {/*{ this.state.wheelNumbers.map(n => <Number key={n} number={n} />)}*/}
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={-4} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={-3} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={-2} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={-1} />
-            <Number className="marker" pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={0} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={1} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={2} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={3} />
-            <Number pos={this.state.currentPos} wheelNumbers={this.state.turnNumbers} offset={4} />
+          {/*{ this.state.wheelNumbers.map(n => <Number key={n} number={n} />)}*/}
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={-4}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={-3}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={-2}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={-1}
+          />
+          <Number
+            className="marker"
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={0}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={1}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={2}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={3}
+          />
+          <Number
+            pos={this.state.currentPos}
+            wheelNumbers={this.state.turnNumbers}
+            offset={4}
+          />
 
-            <div className="timer" style={timerAnimation}></div>
-
+          <div className="timer" style={timerAnimation} />
         </div>
 
         <div className="wheel__container">
-          <div className={`wheel__figure ${wheelFigureClass}`}></div>
+          <div className={`wheel__figure ${wheelFigureClass}`} />
         </div>
-
-
       </div>
     );
   }
